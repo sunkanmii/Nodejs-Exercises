c63e917744b4e4839b589982751cfd4bf3e605a4
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-03-20 23:27:33 +0100

    Mongoose added

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+
+const dbUrl = ""
+
+const Message = mongoose.model("Message", {
+    name: String,
+    message: String
+})
 
 // Messages service endpoint
 const messages = [
@@ -19,6 +27,23 @@ app.get("/messages", (req, res) =>{
 })
 
 app.post("/messages", (req, res) =>{
+    const message = new Message(req.body);
+
+    message.save((err) => {
+        if(err){
+            sendStatus(500);    
+        }
+
+        MessageChannel.findOne({message: 'badword'}, (err, censored) => {
+            if(censored){
+                console.log("Censored words found", censored);
+                Message.remove({_id: censored.id}, (err) => {
+                    console.log("Removed censored message.");
+                })
+            }
+        })
+    })
+
     messages.push(req.body);
     io.emit("message", req.body);
     res.sendStatus(200);
